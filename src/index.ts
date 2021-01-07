43639942f3f8ea635aef554d14db87233307d51d
zak <codedpills@gmail.com>
2021-01-07 16:23:59 +0000

    refactor: extract number[] logic to separate class

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,9 @@
-class Sorter {
-    constructor(public collection: number[] | string) {
-    }
+import { Sorter } from './Sorter';
+import { NumbersCollection } from "./NumbersCollection";
 
-    sort = (): void => {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
+const numbersCollection = new NumbersCollection([-1, 4, 5, 1000, 0, 23])
 
-                // only works if collection is number[]
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        let leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                // only works if collection is string
-                if (typeof this.collection === 'string') {
-                    
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([-1, 4, 5, 0, 23]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort()
 
-console.log(sorter.collection);
+console.log(numbersCollection.data);
