958d4ff62bb0e88a76e8b9a1fa8c4f4b07e8c76e
zak <codedpills@gmail.com>
2021-01-07 17:02:25 +0000

    sort string of characters using bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from "./NumbersCollection";
+import {CharactersCollection} from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([-1, 4, 5, -1000, 0, 23])
+// const numbersCollection = new NumbersCollection([-1, 4, 5, -1000, 0, 23])
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort()
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort()
 
-console.log(numbersCollection.data);
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("aabbsstzzy");
+
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+
+console.log(charactersCollection.data);
