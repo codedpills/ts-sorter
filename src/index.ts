996ca8db186422b522230088af72fc91ad0adcf5
zak <codedpills@gmail.com>
2021-01-07 15:50:00 +0000

    bad code: sorting using OR union in TS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 class Sorter {
-    constructor(public collection: number[]) {
+    constructor(public collection: number[] | string) {
     }
 
     sort = (): void => {
-        const length = this.collection.length;
+        const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    let leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+
+                // only works if collection is number[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        let leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+
+                // only works if collection is string
+                if (typeof this.collection === 'string') {
+                    
                 }
             }
         }
