184b67c6f00d1721826aea8b58f6daa2dd565aa0
zak <codedpills@gmail.com>
2021-01-07 18:33:33 +0000

    refactor collection to extend Sorter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from "./NumbersCollection";
-import {CharactersCollection} from './CharactersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 // const numbersCollection = new NumbersCollection([-1, 4, 5, -1000, 0, 23])
-
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort()
-
+// numbersCollection.sort()
 // console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("aabbsstzzy");
-
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-
+charactersCollection.sort();
 console.log(charactersCollection.data);
