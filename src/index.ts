427b6cfe72c93f84751e80001098d64f08c1c218
zak <codedpills@gmail.com>
2021-01-07 16:41:34 +0000

    refactor sorting to use an interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollection([-1, 4, 5, 1000, 0, 23])
+const numbersCollection = new NumbersCollection([-1, 4, 5, -1000, 0, 23])
 
 const sorter = new Sorter(numbersCollection);
 sorter.sort()
